45cf3dd11c9aba9fba70b08eeac5eeba76045185
bharat <bhushansingh696@gmail.com>
2023-02-12 05:32:29 +0530

    feat: adding some feat

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout({
       <body>
         <section className="p-4">
           <Navbar />
+          <p></p>
           {children}
         </section>
       </body>
